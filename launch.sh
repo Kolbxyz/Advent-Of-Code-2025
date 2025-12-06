d7fdd41602be23a9b9a8980c1240c462b4eb7c14
Kolbxyz <Kolbxyz@outlook.com>
2025-12-06 17:20:25 +0100

    Change launch script

--- launch.sh
+++ launch.sh
@@ -2,6 +2,8 @@
 if [ $# == 2 ]; then
     if [ -f "./2025/Day$1/Task$2/main.lua" ]; then
         lua ./2025/Day$1/Task$2/main.lua
+    elif [ -f "./2025/Day$1/Task$2/main.luau" ]; then
+        luau ./2025/Day$1/Task$2/main.luau
     else
         if [ -d "./2025/Day$1" ]; then
             if [ -d "./2025/Day$1/Task$2/" ]; then
